74fd40eb022bf6858a25eea0a91039312dec20d3
Aziz Alfauzi <azizalfauzi@student.telkomuniversity.ac.id>
2023-09-18 10:04:05 +0700

    * NodeJsCompany - setup database - setup project folder - setup register user - setup test register user

---
+++ tests/test-utils.js
@@ -0,0 +1,22 @@
+import { prismaClient } from "../src/app/database";
+import bcrypt from "bcrypt";
+
+export const removeTestUser = async () => {
+  await prismaClient.user.deleteMany({
+    where: {
+      username: "test",
+    },
+  });
+};
+
+export const createTestUser = async () => {
+  await prismaClient.user.create({
+    data: {
+      username: "test",
+      password: await bcrypt.hash("rahasia", 10),
+      name: "test",
+      role: "admin",
+      token: "test",
+    },
+  });
+};
---
+++ tests/user/register-user.test.js
@@ -0,0 +1,60 @@
+import supertest from "supertest";
+import { createTestUser, removeTestUser } from "../test-utils.js";
+import { web } from "../../src/app/web.js";
+import { logger } from "../../src/app/logging.js";
+
+describe("Register POST /api/users", () => {
+  afterEach(async () => {
+    await removeTestUser();
+  });
+
+  it("should can be register new user", async () => {
+    const result = await supertest(web).post("/api/users").send({
+      username: "test",
+      password: "rahasia",
+      name: "test",
+      role: "admin",
+    });
+
+    expect(result.status).toBe(200);
+    expect(result.body.data.username).toBe("test");
+    expect(result.body.data.name).toBe("test");
+    expect(result.body.data.role).toBe("admin");
+    expect(result.body.data.password).toBeUndefined();
+  });
+
+  it("should error register new user ", async () => {
+    const result = await supertest(web).post("/api/users").send({
+      username: "",
+      password: "",
+      name: "",
+      role: "",
+    });
+
+    expect(result.status).toBe(400);
+  });
+
+  it("should reject if username is already registered", async () => {
+    let result = await supertest(web).post("/api/users").send({
+      username: "test",
+      password: "rahasia",
+      name: "test",
+      role: "admin",
+    });
+    expect(result.status).toBe(200);
+    expect(result.body.data.username).toBe("test");
+    expect(result.body.data.name).toBe("test");
+    expect(result.body.data.role).toBe("admin");
+    expect(result.body.data.password).toBeUndefined();
+    result = await supertest(web).post("/api/users").send({
+      username: "test",
+      password: "rahasia",
+      name: "test",
+      role: "admin",
+    });
+    console.info(result.status);
+    console.info(result.body);
+    expect(result.status).toBe(400);
+    expect(result.body.errors).toBeDefined();
+  });
+});
